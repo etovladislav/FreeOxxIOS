78f5a787184a3d2ed7c8af0bc08147546bec51eb
etovladislav <etovladislav@yandex.ru>
2016-04-23 19:31:47 +0300

    Initial Commit

---
+++ FreeOxx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreeOxx
+//
+//  Created by Владислав Ульянов on 23.04.16.
+//  Copyright © 2016 Владислав Ульянов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreeOxx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FreeOxx
+//
+//  Created by Владислав Ульянов on 23.04.16.
+//  Copyright © 2016 Владислав Ульянов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
